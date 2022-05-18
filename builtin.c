0df8dd2bf7103caebad9cefb2fee22c2649a5f8b
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-05-18 03:26:44 -0400

    builtin added

--- builtin.c
+++ builtin.c
@@ -1,4 +1,3 @@
-#include "shell.h"
 /**
  * exit_bul - Exit Statue Shell
  * @cmd: Parsed Command
@@ -7,6 +6,8 @@
  * @c:Excute Count
  * Return: Void (Exit Statue)
  */
+
+/**
 void  exit_bul(char **cmd, char *input, char **argv, int c)
 {
 	int statue, i = 0;
@@ -33,7 +34,7 @@ void  exit_bul(char **cmd, char *input, char **argv, int c)
 		}
 	}
 }
-
+*/
 
 /**
  * change_dir - Change Dirctorie
@@ -41,6 +42,8 @@ void  exit_bul(char **cmd, char *input, char **argv, int c)
  * @er: Statue Last Command Excuted
  * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
  */
+
+/**
 int change_dir(char **cmd, __attribute__((unused))int er)
 {
 	int value = -1;
@@ -68,12 +71,14 @@ int change_dir(char **cmd, __attribute__((unused))int er)
 	}
 	return (0);
 }
+*/
 /**
  * dis_env - Display Enviroment Variable
  * @cmd:Parsed Command
  * @er:Statue of Last command Excuted
  * Return:Always 0
  */
+/**
 int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
 {
 	size_t i;
@@ -87,12 +92,14 @@ int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int er)
 	}
 	return (0);
 }
+*/
 /**
  * display_help - Displaying Help For Builtin
  * @cmd:Parsed Command
  * @er: Statue Of Last Command Excuted
  * Return: 0 Succes -1 Fail
  */
+/**
 int display_help(char **cmd, __attribute__((unused))int er)
 {
 	int fd, fw, rd = 1;
@@ -116,12 +123,14 @@ int display_help(char **cmd, __attribute__((unused))int er)
 	_putchar('\n');
 	return (0);
 }
+*/
 /**
  * echo_bul - Excute Echo Cases
  * @st:Statue Of Last Command Excuted
  * @cmd: Parsed Command
  * Return: Always 0 Or Excute Normal Echo
  */
+/**
 int echo_bul(char **cmd, int st)
 {
 	char *path;
@@ -151,3 +160,358 @@ int echo_bul(char **cmd, int st)
 
 	return (1);
 }
+*/
+
+
+#include "shell.h"
+
+/* Execution */
+/**
+ * @description:: description of the child
+ * @param:argv:  getting arguments for execvp (int execvp(const char *file, char *const argv[]);)
+ * @return: if success none
+ */
+void exec_child(char **argv) {
+    if (execvp(argv[0], argv) < 0) {
+        fprintf(stderr, "Error: Failed to execte command.\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * @description: description for <
+ * @param:argv:   arguments parse_redirect
+ * @return: if success none
+ */
+void exec_child_overwrite_from_file(char **argv, char **dir) {
+    /* selli>ls < out.txt */
+    int fd_in = open(dir[1], O_RDONLY);
+    if (fd_in == -1) {
+        perror("Error: Redirect input failed");
+        exit(EXIT_FAILURE);
+    }
+
+    dup2(fd_in, STDIN_FILENO);
+
+    if (close(fd_in) == -1) {
+        perror("Error: Closing input failed");
+        exit(EXIT_FAILURE);
+    }
+    exec_child(argv);
+}
+
+/**
+ * @description:: describe the >
+ * @param:argv:  : getting arguments parse_redirect
+ * @return: if success none
+ */
+void exec_child_overwrite_to_file(char **argv, char **dir) {
+    /* selli>ls > out.txt */
+
+    int fd_out;
+    fd_out = creat(dir[1], S_IRWXU);
+    if (fd_out == -1) {
+        perror("Error: Redirect output failed");
+        exit(EXIT_FAILURE);
+    }
+    dup2(fd_out, STDOUT_FILENO);
+    if (close(fd_out) == -1) {
+        perror("Error: Closing output failed");
+        exit(EXIT_FAILURE);
+    }
+
+    exec_child(argv);
+}
+
+/**
+ * @description: description
+ * @param:argv:  arguments to  parse_redirect
+ * @return: if success none
+ */
+void exec_child_append_to_file(char **argv, char **dir) {
+    /* selli>ls >> out.txt */
+    int fd_out;
+    if (access(dir[0], F_OK) != -1) {
+        fd_out = open(dir[0], O_WRONLY | O_APPEND);
+    }
+    if (fd_out == -1) {
+        perror("Error: Redirect output failed");
+        exit(EXIT_FAILURE);
+    }
+    dup2(fd_out, STDOUT_FILENO);
+    if (close(fd_out) == -1) {
+        perror("Error: Closing output failed");
+        exit(EXIT_FAILURE);
+    }
+    exec_child(argv);
+}
+
+/**
+ * @description: describe a Pipe
+ * @param:argv: argument to child 02
+ * @return: if success none
+ */
+void exec_child_pipe(char **argv_in, char **argv_out) {
+    int fd[2];
+    /* p[0]: read end */
+    /* p[1]: write end */
+    if (pipe(fd) == -1) {
+        perror("Error: Pipe failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //child 1 exec input from main process
+    //write to child 2
+    if (fork() == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        exec_child(argv_in);
+        exit(EXIT_SUCCESS);
+    }
+
+    //child 2 exec output from child 1
+    //read from child 1
+    if (fork() == 0) {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[1]);
+        close(fd[0]);
+        exec_child(argv_out);
+        exit(EXIT_SUCCESS);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    wait(0);
+    wait(0);    
+}
+
+/**
+ * @description: 
+ * @param:
+ * @return: if success
+ */
+void exec_parent(pid_t child_pid, int *bg) {}
+
+/* History
+/**
+ * @description: description of the function
+ * @param: history  history, line 
+ * @return: if success none
+ */
+void set_prev_command(char *history, char *line) {
+    strcpy(history, line);
+}
+
+/**
+ * @description: describe it
+ * @param: history , history
+ * @return: if success none
+ */
+char *get_prev_command(char *history) {
+    if (history[0] == '\0') {
+        fprintf(stderr, "No commands in history\n");
+        return NULL;
+    }
+    return history;
+}
+
+/* Built-in: Implement builtin functions để thực hiện vài lệnh cơ bản như cd (change directory), demo custome help command
+/*
+  Function Declarations for builtin shell commands:
+ */
+int simple_shell_cd(char **args);
+int simple_shell_help(char **args);
+int simple_shell_exit(char **args);
+void exec_command(char **args, char **redir_argv, int wait, int res);
+
+/* List of builtin commands */
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+/* Corresponding functions. */
+int (*builtin_func[])(char **) = {
+    &simple_shell_cd,
+    &simple_shell_help,
+    &simple_shell_exit
+};
+
+int simple_shell_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/* Implement - */
+
+/**
+ * @description: describe the cd (change directory) alsochdir()
+ * @param:argv:  getting the arguments
+ * @return: if success 0 if success
+ */
+int simple_shell_cd(char **argv) {
+    if (argv[1] == NULL) {
+        fprintf(stderr, "Error: Expected argument to \"cd\"\n");
+    } else {
+        /* Change the process's working directory to PATH. */
+        if (chdir(argv[1]) != 0) {
+            perror("Error: Error when change the process's working directory to PATH.");
+        }
+    }
+    return 1;
+}
+
+/**
+ * @description: description help in command
+ * @param:argv:  getting the arguments
+ * @return: if success
+ */
+int simple_shell_help(char **argv) {
+    static char help_team_information[] =
+        "OPERATING SYSTEMS PROJECT 01 - A SIMPLE SHELL\n"
+        "λ Team member λ\n"
+        "18120061 \t\tNhut-Nam Le\n"
+        "18120185 \t\tDang-Khoa Doan\n"
+        "λ Description λ\n"
+        "Khoa and Nam's Shell is a simple UNIX command interpreter that replicates functionalities of the simple shell (sh).\n"
+        "This program was written entirely in C as assignment for project 01 in Operating Systems Course CQ2018-21, host by lecturers Dung Tran Trung & lab teacher Le Giang Thanh."
+        "\n"
+        "\nUsage help command. Type help [command name] for help/ more information.\n"
+        "Options for [command name]:\n"
+        "cd <directory name>\t\t\tDescription: Change the current working directory.\n"
+        "exit              \t\t\tDescription: Exit Khoa & Nam'shell, buyback Linux Shell.\n";
+    static char help_cd_command[] = "HELP CD COMMAND\n";
+    static char help_exit_command[] = "HELP EXIT COMMAND\n";
+
+    if (strcmp(argv[0], "help") == 0 && argv[1] == NULL) {
+        printf("%s", help_team_information);
+        return 0;
+    }
+
+    if (strcmp(argv[1], "cd") == 0) {
+        printf("%s", help_cd_command);
+    } else if (strcmp(argv[1], "exit") == 0) {
+        printf("%s", help_exit_command);
+    } else {
+        printf("%s", "Error: Too much arguments.");
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @description:describing the function scenario
+ * @param:args getting the arguments
+ * @return: if success
+ */
+int simple_shell_exit(char **args) {
+    int running;
+    running = 0;
+    return running;
+}
+
+/**
+ * @description:describing the function scenario
+ * @param:
+ * @return: if success
+ */
+int simple_shell_history(char *history, char **redir_args) {
+    char *cur_args[BUFFER_SIZE];
+    char cur_command[MAX_LINE_LENGTH];
+    int t_wait;
+
+    if (history[0] == '\0') {
+        fprintf(stderr, "No commands in history\n");
+        return 1;
+    }
+    strcpy(cur_command, history);
+    printf("%s\n", cur_command);
+    parse_command(cur_command, cur_args, &t_wait);
+    int res = 0;
+    exec_command(cur_args, redir_args, t_wait, res);
+    return res;
+}
+
+
+/**
+ * @description:description IO
+ * @param:args getting the arguments, redir_argv getting the arguments 
+ * @return: if success 0 
+ */
+int simple_shell_redirect(char **args, char **redir_argv) {
+    /* printf("%s", "Executing redirect\n"); */
+    int redir_op_index = is_redirect(args);
+    /* printf("%d", redir_op_index); */
+    if (redir_op_index != 0) {
+        parse_redirect(args, redir_argv, redir_op_index);
+        if (strcmp(redir_argv[0], ">") == 0) {
+            exec_child_overwrite_to_file(args, redir_argv);
+        } else if (strcmp(redir_argv[0], "<") == 0) {
+            exec_child_overwrite_from_file(args, redir_argv);
+        } else if (strcmp(redir_argv[0], ">>") == 0) {
+            exec_child_append_to_file(args, redir_argv);
+        }
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @description: Hàm thực thi pipe
+ * @param: args getting the arguments
+ * @return: if success 0 
+ */
+int simple_shell_pipe(char **args) { 
+    int pipe_op_index = is_pipe(args);
+    if (pipe_op_index != 0) {  
+        /* printf("%s", "Exec Pipe"); */
+        char *child01_arg[PIPE_SIZE];
+        char *child02_arg[PIPE_SIZE];   
+        parse_pipe(args, child01_arg, child02_arg, pipe_op_index);
+        exec_child_pipe(child01_arg, child02_arg);
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @description: description
+ * @param: parameters
+ * @return: if success
+ */
+void exec_command(char **args, char **redir_argv, int wait, int res) {
+
+    for (int i = 0; i < simple_shell_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            (*builtin_func[i])(args);
+            res = 1;
+        }
+    }
+
+    /* builtin commands */
+    if (res == 0) {
+        int status;
+
+        /* forking */
+        pid_t pid = fork();
+        if (pid == 0) {
+            /* Child process */
+            if (res == 0) res = simple_shell_redirect(args, redir_argv);
+            if (res == 0) res = simple_shell_pipe(args);
+            if (res == 0) execvp(args[0], args);
+            exit(EXIT_SUCCESS);
+
+        } else if (pid < 0) { /* if any error */
+            perror("Error: Error forking");
+            exit(EXIT_FAILURE);
+        } else { 
+            /* Parent process */
+            /* printf("[LOGGING] Parent pid = <%d> spawned a child pid = <%d>.\n", getpid(), pid); */
+            if (wait == 1) {
+                waitpid(pid, &status, WUNTRACED);  
+            }
+        }
+    }
+}
+
