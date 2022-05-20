b9c01b18105c9c1bde374efbdcc86e4d1937b7cc
Kalrul <prizsabertler@gmail.comgit config --global user.email prizsabertler@gmail.com>
2022-05-20 01:08:23 -0700

    header file

--- shell.h
+++ shell.h
@@ -3,73 +3,54 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
 
-#define BUFFER 1024
-#define TRUE 1
-#define PROMPT "$ "
-/* error strings */
-#define ERR_MALLOC "Unable to malloc space\n"
-#define ERR_FORK "Unable to fork and create child process\n"
-#define ERR_PATH "No such file or directory\n"
 extern char **environ;
+extern int errno;
 
 /**
- * struct list_s - linked list of variables
- * @value: value
- * @next: pointer to next node
- *
- * Description: generic linked list struct for variables.
-**/
-typedef struct list_s
+ * struct selectfunction - a struct array of functions
+ * @command: a pointer to the caharacter of the command string
+ * @funcptr: a pointer to a function
+ */
+
+typedef struct selectfunction
 {
-	char *value;
-	struct list_s *next;
-} list_s;
+	char *command;
+	int (*funcptr)(char **line);
+} selecte;
 
-/**
- * struct built_s - linked list of builtins
- * @name: name of builtin
- * @p: pointer to function
- *
- * Description: struct for builtin functions.
-**/
-typedef struct built_s
-{
-	char *name;
-	int (*p)(void);
-} built_s;
+void sigintHandler(int sig_num __attribute__((unused)));
+void printprompt(int i);
+int exitor(char *line[]);
+int cater(char *line[]);
+int getstr(char *str);
+int shellprocessor(char **line, char **argv);
+char **strbrk(char *line, char c);
+int _getline(char **line, size_t *len);
+void farewell(void);
+void description(void);
+void printstar(void);
+void printstr(char *s);
+int strleng(char *s);
+void strcopy(char *src, char *dest);
+int strcomp(char *str, char *equ);
+void strmix(char *src, char *dest);
+int lister(char **line);
+int (*getfunc(char *str))(char **line);
+int echorr(char *line[]);
+int builtincom(char **line);
+int changedire(char **line);
+int pwder(char *line[]);
+int envir(char *line[]);
+int maker(char *line[]);
+char _getchar(void);
+char *stringer(char *line, int i, char *str, char c);
 
-void prompt(int fd, struct stat buf);
-char *_getline(FILE *fp);
-char **tokenizer(char *str);
-char *_which(char *command, char *fullpath, char *path);
-int child(char *fullpath, char **tokens);
-void errors(int error);
-
-/* utility functions */
-void _puts(char *str);
-int _strlen(char *s);
-int _strcmp(char *name, char *variable, unsigned int length);
-int _strncmp(char *name, char *variable, unsigned int length);
-char *_strcpy(char *dest, char *src);
-
-/* prototypes for builtins */
-int shell_env(void);
-int shell_exit(void);
-int builtin_execute(char **tokens);
-int shell_num_builtins(built_s builtin[]);
-
-/* prototypes for the helper functions for path linked list */
-char *_getenv(const char *name);
-char **copy_env(char **environ_copy, unsigned int environ_length);
-list_s *pathlist(char *variable, list_s *head);
-
-/* prototypes for free functions */
-void free_all(char **tokens, char *path, char *line, char *fullpath, int flag);
-void free_dp(char **array, unsigned int length);
-#endif /* SHELL_H */
+#endif
